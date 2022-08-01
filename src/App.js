546a1a0264b380cf5714b17ab401d1190a35a3a3
md sanowar alam <94783739+Sanowar72@users.noreply.github.com>
2022-08-01 11:38:03 +0530

    react practice
    i did practice on react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <>
+    <Home />
+    
+    </>
+  )
+}
+
+export default App
